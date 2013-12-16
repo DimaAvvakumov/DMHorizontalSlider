3e8688a72e0d1604082e92efb01a36a97c3ca0ea
Dima Avvakumov <avvakumov@east-media.ru>
2013-12-16 14:08:12 +0600

    Nib cell added

---
+++ classes/classes/Cell/SliderNibCellView.h
@@ -0,0 +1,16 @@
+//
+//  SliderNibCellView.h
+//  DMHorizontalSlider
+//
+//  Created by Dima Avvakumov on 16.12.13.
+//  Copyright (c) 2013 Dima Avvakumov. All rights reserved.
+//
+
+#import "DMHorizontalSliderCell.h"
+
+@interface SliderNibCellView : DMHorizontalSliderCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+
+
+@end
---
+++ classes/classes/Cell/SliderNibCellView.m
@@ -0,0 +1,13 @@
+//
+//  SliderNibCellView.m
+//  DMHorizontalSlider
+//
+//  Created by Dima Avvakumov on 16.12.13.
+//  Copyright (c) 2013 Dima Avvakumov. All rights reserved.
+//
+
+#import "SliderNibCellView.h"
+
+@implementation SliderNibCellView
+
+@end
